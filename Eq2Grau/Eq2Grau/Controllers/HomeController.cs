80779ffbcb361856341c6a889acf9f6769680536
Xyrsto <rodrigtomas@gmail.com>
2023-03-23 13:03:03 +0000

    Backend is working! Needs bug fixing tho.

--- Eq2Grau/Eq2Grau/Controllers/HomeController.cs
+++ Eq2Grau/Eq2Grau/Controllers/HomeController.cs
@@ -13,10 +13,34 @@ namespace Eq2Grau.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int a, int b, int c)
         {
+            double delta = b * b - 4 * a * c;
+            string x1 = "", x2 = "";
+            //posso calcular as minhas raízes?
+            if(a != 0)
+            {
+                if(delta >= 0) 
+                {
+                    x1 = ((-b + Math.Sqrt(delta)) / (2 * a)).ToString();
+                    x2 = ((-b - Math.Sqrt(delta)) / (2 * a)).ToString();
+                }
+                else
+                {
+                    x1 = ((-b / (2 * a) + Math.Sqrt(Math.Abs(delta))) / (2 * a)).ToString() + "i";
+                    x2 = ((-b / (2 * a) - Math.Sqrt(Math.Abs(delta))) / (2 * a)).ToString() + "i";
+                }
+
+            }
+
+            //preparar os dados para enviar para a vista
+            ViewBag.x1 = x1;
+            ViewBag.x2 = x2;
+
             return View();
         }
+
+        
 
         public IActionResult Privacy()
         {
